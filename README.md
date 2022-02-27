c8aa0454620c112593005a103c7ddf66dc4bb6fc
Alexander Payne <81712870+DrPayne25@users.noreply.github.com>
2022-02-26 16:17:25 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# django-snacks
+# Lab 26 Caesar Cipher
+
+**Author**: Alex Payne
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)  
+**PR**: https://github.com/DrPayne25/caesar-cipher/pull/1
+
+## Overview
+This is the start of a snacks web page that will have a few different pages. 
+
+## Getting Started
+1. Create a new poetry project this one is called Django-Snacks
+2. Setup as normal per this guide https://codefellows.github.io/code-401-python-guide/reference/submission-instructions/labs/
+3. Once done poetry add django
+4. Create a new module project called django_snacks_project
+
+## Architecture
+
+## Change Log
+### Name of feature: Setup About, Snacks, Home pages
+
+Estimate of time needed to complete: 1hr 
+
+Start time: 1:15
+
+Finish time: 4:16
+
+Actual time needed to complete: 3hrs
+
+## Credits 
+Roger
--- snacks_app/tests.py
+++ snacks_app/tests.py
@@ -23,6 +23,16 @@ class SnacksTests(SimpleTestCase):
     response = self.client.get(url)
     self.assertNotEqual(response.status_code, 300)
 
+  def test_snacks_page_status(self):
+    url = reverse('snacks')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+  def test_snacks_page_status_not_working(self):
+    url = reverse('snacks')
+    response = self.client.get(url)
+    self.assertNotEqual(response.status_code, 300)
+
   def test_home_base_template(self):
     url = reverse('home')
     response = self.client.get(url)
@@ -45,4 +55,16 @@ class SnacksTests(SimpleTestCase):
     url = reverse('about')
     response = self.client.get(url)
     self.assertTemplateNotUsed(response, 'abouts.html')
+    self.assertTemplateNotUsed(response, 'bases.html')
+
+  def test_snacks_base_template(self):
+    url = reverse('snacks')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'snacks.html')
+    self.assertTemplateUsed(response, 'base.html')
+
+  def test_snacks_base_template_not_working(self):
+    url = reverse('snacks')
+    response = self.client.get(url)
+    self.assertTemplateNotUsed(response, 'snack.html')
     self.assertTemplateNotUsed(response, 'bases.html')
